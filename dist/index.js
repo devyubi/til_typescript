f6e07536ff5fc7aa7d1bea3686de98c98c5c32c2
devmunyubi <dev.munyubi@gmail.com>
2025-06-26 13:17:57 +0900

    [docs] class 기초

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,19 @@
+class Animal {
+  constructor() {
+    this.eye = 2;
+  }
+  cry() {}
+  eat() {}
+}
+class Cat extends Animal {
+  constructor() { //constructor 이거 안적어도 되는데 그냥 적음. (안적어도 실행 된다!!)
+    super(); // 맨~~~위 애니멀의 컨스트럭터를 실행하라는 뜻
+  }
+  꾹꾹이() {}
+}
+class Dog extends Animal {
+  constructor() {}
+  멍멍() {}
+}
 
+new Cat();
